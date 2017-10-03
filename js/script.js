548d5927a838193cda223a38a48322ea1e6d9b14
lucas-str <lucas.sturelle@yahoo.fr>
2017-10-03 08:58:39 +0200

    Create classes in separate files

--- js/script.js
+++ js/script.js
@@ -1,35 +1,3 @@
-function Service(name,capacity, string_object)
-{
-    this.name = name;
-    this.capacity = capacity;
-    this.occupancy = 0;
-    this.string_object = string_object;
-}
-
-Service.prototype.addPersons = function(n)
-{
-    this.occupancy += n;
-}
-
-Service.prototype.Update_String = function()
-{
-    this.string_object.innerHTML =     this.name
-                    +":\t"
-                    +this.occupancy
-                    +"/"
-                    +this.capacity;
-                
-}
-
-function Vehicule(name, capacity, path, string_object)
-{
-    Service.call(this, name, capacity, string_object);
-    this.path = path;
-}
-
-Vehicule.prototype = Object.create(Service.prototype);
-Vehicule.prototype.constructor = Vehicule;
-
 document.addEventListener("DOMContentLoaded", function(event)
 {
     svg = document.getElementById("campus");
@@ -57,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function(event)
 
 });
 
-function Bus(name, capacity, path, string_object)
-{
-    Vehicule.call(this, name, capacity, path, string_object);
-}
+//function Bus(name, capacity, path, string_object)
+//{
+    //Vehicule.call(this, name, capacity, path, string_object);
+//}
